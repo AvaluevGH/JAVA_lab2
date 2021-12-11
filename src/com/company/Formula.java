f01dce2b69ea4b2b4203622cbe3110f8bd0d273e
AvaluevGH <anton.valuev.99@mail.ru>
2021-12-11 22:44:59 +0300

    Initial commit

---
+++ src/com/company/Formula.java
@@ -0,0 +1,16 @@
+
+    package com.company;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+    // Главный класс приложения, он же класс фрейма
+    public class Formula extends JFrame {
+        private Double result = 0.0;
+        private Double sum = 0.0;
+        private int formula = 0;
+
+
+
+    }
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+                com.company.Formula formula = new Formula();
+                formula.setVisible(true); //Для отображения окна используется метод
+            }
+        }
+
+
